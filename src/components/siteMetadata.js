7fdd99e6eaa740ab74dcf242cfc626daee366da1
cosmicsarthak <sarthakmohanty200@gmail.com>
2025-03-05 02:15:38 +0530

    for backup

---
+++ src/components/siteMetadata.js
@@ -0,0 +1,22 @@
+const metadata = (params) => ({
+  description: "Made in Framer",
+  favicon: new URL("assets/tfgNOJnlIsuPwS55P7pcyPDyHuU.png", import.meta.url)
+    .href,
+  socialImage: new URL(
+    "assets/ThoIAyvTheUNQRX6DYh0iCyXJag.png",
+    import.meta.url,
+  ).href,
+  title: "AI Button",
+});
+export default metadata;
+export const metadataVersion = 1;
+export const __FramerMetadata__ = {
+  exports: {
+    metadataVersion: {
+      type: "variable",
+      annotations: { framerContractVersion: "1" },
+    },
+    default: { type: "variable", annotations: { framerContractVersion: "1" } },
+    __FramerMetadata__: { type: "variable" },
+  },
+};
